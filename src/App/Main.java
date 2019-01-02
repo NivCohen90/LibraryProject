1809e8ac0012f056db8f703a0493cc1ef7873901
alexe <alexe@LAPTOP-PHHL7VUV>
2019-01-02 22:52:23 +0200

    heyyy comment in app>main

--- src/App/Main.java
+++ src/App/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
-public class Main extends Application {
+public class Main extends Application {//Heyyyyyyyyyy
 
 	@Override
 	public void start(Stage primaryStage) {
