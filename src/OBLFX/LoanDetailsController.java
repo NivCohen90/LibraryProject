6161131ecf8342c24a848c4b0aecb7c9d4b3291b
liadb <liadb@10.10.0.69>
2019-02-02 00:11:38 +0200

    00:11, 2/2

---
+++ src/OBLFX/LoanDetailsController.java
@@ -0,0 +1,79 @@
+package OBLFX;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoanDetailsController {
+
+	  @FXML
+	    private Label LoanIDLable;
+
+	    @FXML
+	    private TextField LoanIDTXT;
+
+	    @FXML
+	    private Label SubscriberIDLable;
+
+	    @FXML
+	    private TextField SubscriberIDTXT;
+
+	    @FXML
+	    private Label StudentIDLable;
+
+	    @FXML
+	    private TextField StudentIDTXT;
+
+	    @FXML
+	    private Label LoanStatusLable;
+
+	    @FXML
+	    private TextField LoanStatusTXT;
+
+	    @FXML
+	    private Label StartDateLable;
+
+	    @FXML
+	    private DatePicker StartDateCombo;
+
+	    @FXML
+	    private Label CopyIDLable;
+
+	    @FXML
+	    private TextField CopyIDTXT;
+
+	    @FXML
+	    private Label BookNameLable;
+
+	    @FXML
+	    private TextField BookNameTXT;
+
+	    @FXML
+	    private Label BookAuthorLable;
+
+	    @FXML
+	    private TextField BookAuthorTXT;
+
+	    @FXML
+	    private Label BookCatalogNumberLable;
+
+	    @FXML
+	    private TextField BookCatalogNumberTXT;
+
+	    @FXML
+	    private Label returnDateLable;
+
+	    @FXML
+	    private DatePicker ReturnDateCombo;
+
+	    @FXML
+	    private Button ExtendLoanButton;
+	    
+	    
+	    
+	    public void SubscriberExtendLoan() {
+	    	
+	    }
+}
